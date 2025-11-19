4436c0ddafaec9340c0cd5283a5abd1d75afbdfc
Jeff Trevino <jeffrey.trevino@gmail.com>
2025-11-19 01:01:38 -0800

    added comment to solution

--- poly_efficient.py
+++ poly_efficient.py
@@ -5,8 +5,7 @@ class EfficientPolyphonyHandler:
     
     def __init__(self, list_of_command_lists):
         self.list_of_command_lists = list_of_command_lists
-        self.active_notes = set()
-        self.current_list_index = 0
+        self.active_notes = set() # a set is a hash table with keys and no values
     
     def process_command(self, command):
         """Process a single command to update active notes."""
@@ -16,11 +15,9 @@ class EfficientPolyphonyHandler:
         if velocity > 0:  # Note ON
             self.active_notes.add(key_number)  # O(1)
         else:  # Note OFF
-            self.active_notes.discard(key_number)  # O(1)
-    
-    def process_all_lists(self):
-        """Process all command lists."""
-        for command_list in self.list_of_command_lists:
-            for command in command_list:
-                self.process_command(command)
-        return sorted(self.active_notes)
+            self.active_notes.discard(key_number)  # O(1) 
+
+    def process_command_list(self, command_list):
+        for command in command_list:
+            self.process_command(command)
+        return self.active_notes.copy() 
